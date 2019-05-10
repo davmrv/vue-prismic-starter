a36d536081aab7f14ade7a72c35280873ce79279
David Maravilla <bl4ckf4ll@gmail.com>
2019-05-10 10:57:01 -0600

    🎉 Init

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
-import Vue from 'vue'
-import PrismicVue from 'prismic-vue'
-import linkResolver from './prismic/link-resolver'
-import htmlSerializer from './prismic/html-serializer'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import PrismicVue from 'prismic-vue';
+import linkResolver from './prismic/link-resolver';
+import htmlSerializer from './prismic/html-serializer';
+import App from './App.vue';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.use(PrismicVue, {
-  endpoint: window.prismic.endpoint,
+  endpoint: process.env.VUE_APP_PRISMIC_REPOSITORY,
   linkResolver,
-  htmlSerializer
-})
+  htmlSerializer,
+});
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <h1>Testing</h1>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+  };
+</script>
